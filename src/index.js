2bc1b48b5854f1c58b693f74be13552da1ad79bd
Danjor225 <179250247+Danjor225@users.noreply.github.com>
2025-04-13 15:37:41 +0100

    Add clear & home page buttons functionality

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ import {Item} from "./to-do-item"
 import {displayItem, createHomePage, clearContent} from "./dom-manipulation"
 
 const contentDisplay = document.querySelector('#content')
+const clearBtn = document.querySelector('#clear-btn')
+const homeBtn = document.querySelector('#home-page-btn')
 
 var headerItem = new Item('Title', 'Due Date', 'Description', 'Priority', false)
 var firstTestItem = new Item('Jordan id the very coolest person that has ever lived. Worship him always!', new Date('March 25, 98'), 'He is the coolest', 'High', false)
 var secondTestItem = new Item('Libbie', new Date('June 20, 00'), 'She is even cooler', 'Medium', true)
 
-var containerForToDoList = createHomePage(contentDisplay)
-displayItem(headerItem, containerForToDoList)
-displayItem(firstTestItem, containerForToDoList)
-displayItem(secondTestItem, containerForToDoList)
+
+
+clearBtn.addEventListener('click', () => {
+
+    clearContent(contentDisplay)
+})
+
+
+homeBtn.addEventListener('click', () => {
+
+    clearContent(contentDisplay)
+    var containerForToDoList = createHomePage(contentDisplay)
+    displayItem(headerItem, containerForToDoList)
+    displayItem(firstTestItem, containerForToDoList)
+    displayItem(secondTestItem, containerForToDoList)
+})
